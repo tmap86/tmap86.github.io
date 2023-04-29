8905b21e951d41e81a7fb6f03e76748fb419d076
tmap86 <96813266+tmap86@users.noreply.github.com>
2023-04-28 21:38:30 -0600

    Delete storescript.js

--- storescript.js
+++
@@ -1,8 +0,0 @@
-const imageContainer = document.querySelector('.image-container');
-const hologram = document.querySelector('.hologram');
-
-imageContainer.addEventListener('mousemove', (event) => {
-  const xPos = event.pageX - imageContainer.offsetLeft;
-  const yPos = event.pageY - imageContainer.offsetTop;
-  hologram.style.transform = `translate(${xPos}px, ${yPos}px)`;
-});
